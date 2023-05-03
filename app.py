60eb147edfb55e7503617e48bc502d42a64ea7a7
divy-a <121391127+divy-a@users.noreply.github.com>
2023-05-03 12:34:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import discord
 import os
+import threading
 from dotenv import load_dotenv
 from flask import Flask
 
 load_dotenv()
 
 app = Flask(__name__)
+
+@app.after_request
+def add_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/')
+def index():
+    return 'BOTTY SERVER'
 
 intents = discord.Intents.default()
 intents.members = True
@@ -22,18 +32,12 @@ async def on_message(message):
         return
 
     if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-        
-client.run(os.getenv('TOKEN'))
+        await message.channel.send('hi')
 
-@app.route('/')
-def index():
-    return 'BOTTY SERVER'
-
-@app.after_request
-def add_headers(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+def start_discord_bot():
+    client.run(os.getenv('TOKEN'))
 
 if __name__ == '__main__':
+    discord_thread = threading.Thread(target=start_discord_bot)
+    discord_thread.start()
     app.run()
